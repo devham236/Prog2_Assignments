fc8fd560d058e6ebe23fc65ff397078603fc2ec4
Hamza Mirza <hamza665@hotmail.de>
2026-04-10 13:28:50 +0200

    task 2a finished

---
+++ src/IntList.java
@@ -0,0 +1,14 @@
+public class IntList {
+    public static void main(String[] args){
+        System.out.println(countNumbers(123));
+    }
+
+    public static int countNumbers(int num){
+        String str = String.valueOf(num);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+        }
+        return count;
+    }
+}
