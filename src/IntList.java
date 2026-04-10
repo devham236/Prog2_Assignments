82d5ec4e0401c5b4c5988a4ca2931c3ff9362088
Hamza Mirza <hamza665@hotmail.de>
2026-04-10 14:04:48 +0200

    task 2b finished

--- src/IntList.java
+++ src/IntList.java
@@ -1,6 +1,10 @@
 public class IntList {
     public static void main(String[] args){
-        System.out.println(countNumbers(123));
+        int[] test = intToList(12345667);
+
+        for (int i = 0; i < test.length; i++) {
+            System.out.printf("%d ", test[i]);
+        }
     }
 
     public static int countNumbers(int num){
@@ -12,9 +16,19 @@ public class IntList {
         return count;
     }
 
+    public static int getDigit(String str, int index){
+        char chr = str.charAt(index);
+        int singleInt = Integer.parseInt(String.valueOf(chr));
+        return singleInt;
+    }
+
     public static int[] intToList(int value){
         int[] list = new int[countNumbers(value)];
-
+        String str = String.valueOf(value);
+        for (int i = 0; i < str.length(); i++) {
+            int digit = getDigit(str, i);
+            list[i] = digit;
+        }
         return list;
     }
 }
